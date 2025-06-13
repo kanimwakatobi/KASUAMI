a936f5127d86cdc0b81c05a98bd4d290fae5395a
kanimwakatobi <kanimwakatobi@gmail.com>
2025-06-13 11:18:58 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const links = document.querySelectorAll(".nav-link");
+  links.forEach(link => {
+    link.addEventListener("click", function () {
+      links.forEach(el => el.classList.remove("active"));
+      this.classList.add("active");
+    });
+  });
+});
