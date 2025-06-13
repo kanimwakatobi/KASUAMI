b06b57dd0eab7211e4f54266630bcc3047b062d8
kanimwakatobi <kanimwakatobi@gmail.com>
2025-06-13 11:18:02 +0800

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const links = document.querySelectorAll(".nav-link");
-  links.forEach(link => {
-    link.addEventListener("click", function () {
-      links.forEach(el => el.classList.remove("active"));
-      this.classList.add("active");
-    });
-  });
-});
